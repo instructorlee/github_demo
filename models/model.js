147215155350e44c0f0d5b384a8438284ec30dcf
kleelof <kleelof@yahoo.com>
2023-05-11 17:48:32 -0700

    update models

--- models/model.js
+++ models/model.js
@@ -4,7 +4,7 @@ const UserSchema = new mongoose.Schema({
     name: {
         type: String,
         required: [true, "Name is Required"],
-        minLength: [3, "Name must be at least 4 characters"]
+        minLength: [3, "Name must be at Least 3 Characters"]
     },
     age: {
         type: Number
